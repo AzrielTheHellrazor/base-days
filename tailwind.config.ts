878525ac9defeac5cc498de98b6327780e5e5c9c
AzrielTheHellrazor <goktug200402@gmail.com>
2024-12-16 20:54:29 +0300

    add publics

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        finger: ["Finger Paint", "cursive"],
+      },
     },
   },
   plugins: [],
